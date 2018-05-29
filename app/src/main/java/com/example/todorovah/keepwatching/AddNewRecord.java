881dc87376456a101dde6246c6f3641b819fe668
Hristina Todorova <todorovah@vmware.com>
2018-05-29 23:04:08 +0300

    work with buttons

--- app/src/main/java/com/example/todorovah/keepwatching/AddNewRecord.java
+++ app/src/main/java/com/example/todorovah/keepwatching/AddNewRecord.java
@@ -1,22 +1,45 @@
 package com.example.todorovah.keepwatching;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.widget.RadioGroup;
 
-public class AddNewRecord extends AppCompatActivity {
+public class AddNewRecord extends AppCompatActivity implements View.OnClickListener {
+
+    Button saveButton;
+    RadioGroup radioGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_record);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        saveButton = findViewById(R.id.save_button);
+        saveButton.setOnClickListener(this);
+
+        //let movie be default choice
+        radioGroup = findViewById(R.id.radioGroup);
+        radioGroup.check(R.id.movieButton);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
+    @Override
+    public void onClick(View view) {
+        int radioButtonId = radioGroup.getCheckedRadioButtonId();
+        if(radioButtonId == R.id.movieButton){
+            Toast.makeText(this, "Movie saved!", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "TV show saved!", Toast.LENGTH_SHORT).show();
+
+        }
+    }
+
 
 }
