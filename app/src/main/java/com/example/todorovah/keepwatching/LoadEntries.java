3280c81ca94d699548fb53fa3ffbfad610eaa5a3
Hristina Todorova <todorovah@vmware.com>
2018-06-08 09:51:05 +0300

    display database content of front page

--- app/src/main/java/com/example/todorovah/keepwatching/LoadEntries.java
+++ app/src/main/java/com/example/todorovah/keepwatching/LoadEntries.java
@@ -1,40 +1,49 @@
 package com.example.todorovah.keepwatching;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import java.util.ArrayList;
+import android.widget.ListView;
 
 public class LoadEntries extends AppCompatActivity {
 
     public static final String DATABASE_NAME = "notes";
 
     SQLiteDatabase myDatabase;
+    ListView myListView;
+    ArrayAdapter<String> listAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_load_entries);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         myDatabase = openOrCreateDatabase(DATABASE_NAME, MODE_PRIVATE, null);
         createMoviesTable();
         createTvShowsTable();
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        myListView = findViewById(R.id.contentsListView);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(view.getContext(), AddNewRecord.class));
             }
         });
+
+        getAllEntries();
     }
 
     private void createMoviesTable() {
@@ -57,6 +66,21 @@ public class LoadEntries extends AppCompatActivity {
                 ");";
 
         myDatabase.execSQL(sql);
+    }
+
+    private void getAllEntries() {
+        ArrayList<String> moviesList = new ArrayList<>();
+        listAdapter = new ArrayAdapter<>(this, R.layout.database_entry_view, moviesList);
+
+        String sql = "SELECT title, timestamp, notes FROM tv_shows;";
+        Cursor cursor = myDatabase.rawQuery(sql, null);
+        if(cursor.moveToFirst()) {
+            do {
+                listAdapter.add(cursor.getString(0));
+            } while(cursor.moveToNext());
+        }
+
+        myListView.setAdapter(listAdapter);
     }
 
     @Override
